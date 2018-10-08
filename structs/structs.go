e9fa0eda64e39ad36bd8db7b183cb98ab432cd73
emile <hanemile@protonmail.com>
2018-10-08 15:30:27 +0200

    Simple package description

--- structs/structs.go
+++ structs/structs.go
@@ -1,5 +1,10 @@
 package structs
 
+/*
+	The structs package defines structs that are used to store information that is used for processing
+	star related data.
+*/
+
 // Force struct soring a force vector
 type Force struct {
 	X, Y float64
