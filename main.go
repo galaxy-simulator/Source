030b1bd41f502f25e6354760405a6acc2a7ee761
emile <hanemile@protonmail.com>
2018-10-08 15:44:22 +0200

    Testing the force.ForceActing(...) function using two manually create stars.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"./forces"
 	"./structs"
 	"fmt"
 )
@@ -9,7 +10,9 @@ func main() {
 	// the slice starsSlice stores the star structures
 	starsSlice := []structs.Star{
 		{structs.Coord{X: 100, Y: 100}, structs.Force{0, 0}, 1000},
+		{structs.Coord{X: 200, Y: 300}, structs.Force{0, 0}, 1000},
 	}
 
 	fmt.Println(starsSlice)
+	fmt.Println(forces.ForceActing(starsSlice[0], starsSlice[1]))
 }
