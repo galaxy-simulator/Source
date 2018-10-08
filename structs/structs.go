5ca2e75f806254e719e90562fbaf4812c5f5f4c5
emile <hanemile@protonmail.com>
2018-10-08 15:25:21 +0200

    Commented Code

--- structs/structs.go
+++ structs/structs.go
@@ -1,13 +1,16 @@
 package structs
 
+// Force struct soring a force vector
 type Force struct {
 	X, Y float64
 }
 
+// Coord struct storing coordinates
 type Coord struct {
 	X, Y float64
 }
 
+// Star struct storing all necessary star information
 type Star struct {
 	C    Coord
 	F    Force
