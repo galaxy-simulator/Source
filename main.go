ab15a949000a8715cd1e6a558bdfd09e306022f4
emile <hanemile@protonmail.com>
2018-10-10 21:47:53 +0200

    Cleand up main

--- main.go
+++ main.go
@@ -19,20 +19,12 @@ func main() {
 		{structs.Coord{X: -30000, Y: 0}, structs.Force{0, 0}, 500000000},
 		{structs.Coord{X: 30000, Y: -30000}, structs.Force{0, 0}, 500000000},
 	}
-	// var starsSlice []structs.Star
 
-	// llog.Good("Opening the csv")
-	// starsSlice = csv.Import("data/structure03.ita.uni-heidelberg.de_26635.csv", 0, 2000, starsSlice)
+	llog.Good("Opening the csv")
 	starsSlice = csv.Import("data/U_ALL.csv", 0, 50000, starsSlice)
-	// fmt.Printf("Done\n")
 
-	// llog.Good("Calculate the acting forces")
+	llog.Good("Calculate the acting forces")
 	starsSlice = forces.CalcAllForces(starsSlice, threads)
-	// starsSlice = forces.CalcAllForcesOld(starsSlice)
-
-	// for _, e := range starsSlice {
-	// 	fmt.Printf("%f %f | %f %f | %f\n", e.C.X, e.C.Y, e.F.X, e.F.Y, e.Mass)
-	// }
 
 	path := "out_2.png"
 
