b15cce5e209012a817dc60055920e958d0d3adc0
emile <hanemile@protonmail.com>
2018-10-11 18:44:55 +0200

    Great function for stuff that is working great

--- llog/llog.go
+++ llog/llog.go
@@ -7,12 +7,16 @@ import (
 
 // Good prints a "good" (green) message with a timestamp and the given message
 func Good(before string) {
-	fmt.Printf("%s\033[36m [+] \033[0m%-60s\t", currentTime(), before)
+	fmt.Printf("%s\033[36m [+] \033[0m%-60s\n", currentTime(), before)
+}
+
+func Great(before string) {
+	fmt.Printf("%s\033[34m [+] \033[0m%-60s\n", currentTime(), before)
 }
 
 // Bad prints a "good" (green) message with a timestamp and the given message
 func Bad(text string) {
-	fmt.Printf("%s\033[31m [+] \033[0m%-60s\t", currentTime(), text)
+	fmt.Printf("%s\033[31m [+] \033[0m%-60s", currentTime(), text)
 }
 
 // current_time returns the current time as a string in the HH:MM:SS format
