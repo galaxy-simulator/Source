b9700e6860cddabc0c3e0d85344863fc7057af36
emile <hanemile@protonmail.com>
2018-10-13 22:40:46 +0200

    changed the amount of stars generated

--- main.go
+++ main.go
@@ -17,23 +17,15 @@ func main() {
 	starsSlice := []structs.Star{}
 
 	llog.Good("Opening the csv")
-	starsSlice = csv.Import("data/U_ALL.csv", 0, 25000, starsSlice)
+	starsSlice = csv.Import("data/U_ALL.csv", 0, 2500, starsSlice)
 
-	// Step 1
-	llog.Good("Calculate the acting forces")
-	starsSlice = forces.CalcAllForces(starsSlice, threads)
-
-	llog.Good(fmt.Sprintf("draw the slice and save it to %s\n", path1))
-	draw.Slice(starsSlice, path1)
-
-	// Step 2
 	// Simulate the position of the stars after a specific time
 	for i := 0; i < frames; i++ {
 		llog.Great("--- --- --- --- ---")
 		llog.Great(fmt.Sprintf("Frames %d/%d", i, frames))
 
 		llog.Good("Calculate the new Star positions")
-		starsSlice = forces.NextTimestep(starsSlice, 250000)
+		starsSlice = forces.NextTimestep(starsSlice, 500000)
 
 		llog.Good("Calculate the acting forces")
 		starsSlice = forces.CalcAllForces(starsSlice, threads)
